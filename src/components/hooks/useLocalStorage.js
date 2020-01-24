4cc232273d617b49a44d9c88fcc7f94c27af02b1
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-01-23 19:22:39 -0500

    forgot to push my code

--- src/components/hooks/useLocalStorage.js
+++ src/components/hooks/useLocalStorage.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-export default uselocalStorage(key, initialValue){
-    const[storedValue, setStoredValue]=useState(()=>{
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
 
-    })
-
+    return [storedValue, setValue];
 };
